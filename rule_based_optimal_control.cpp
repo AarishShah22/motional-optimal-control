185fe72c6fd8aa5be7c347dbc4f9ea36c596da65
Aarish Shah <shahaarish00@gmail.com>
2025-05-16 12:40:17 -0400

    finished coding the optimization problem

--- rule_based_optimal_control.cpp
+++ rule_based_optimal_control.cpp
@@ -10,13 +10,26 @@ Eigen::Vector3f Agent::getPose() const {
     return pose_;
 } 
 
-EgoVehicle::EgoVehicle() {
+EgoVehicle::EgoVehicle(Eigen::Vector3f init_pose, std::vector<Eigen::Vector3f>& ref_traj, 
+                        std::vector<Agent*>& agents, double dt) : 
+                        init_pose_(init_pose), ref_traj_(ref_traj), 
+                        agents_(agents), dt_(dt) {   
     rear_axle_to_COG_ = 1.0F;
     length_of_vehicle_ = 2.0F;
-    Eigen::Vector3f pose1{1.0F,1.0F,1.0F};
-    Agent* agent1 = new Agent(pose1);
-    agents_.push_back(agent1);
-}
+    num_timesteps_ = ref_traj_.size();
+    // priority_structure_ = 
+    eps_ = 1.5;
+    gamma_1_ = 0.1;
+    gamma_2_ = 0.1;
+    gamma_3_ = 0.1;
+    r_ = 1;
+    cost_coeffs_ = std::vector<double>{0.1,0.1,0.01,100,1.5,0.1};
+    x_veh_ = std::vector<double>(num_timesteps_);
+    y_veh_ = std::vector<double>(num_timesteps_);
+    theta_veh_ = std::vector<double>(num_timesteps_);
+    error_ = std::vector<double>(num_timesteps_);
+    ped_pose_ = agents_[0]->getPose();
+} 
 
 EgoVehicle::~EgoVehicle() {
     for (auto agent : agents_) {
@@ -25,35 +38,38 @@ EgoVehicle::~EgoVehicle() {
     agents_.clear();
 }
 
-void EgoVehicle::setReferenceTrajectory(std::vector<Eigen::Vector3f>& ref_traj) {
-    ref_traj_ = ref_traj;
+void EgoVehicle::computeTrajectory(const double* x) {
+    const double* v = x;
+    const double* omega = x + num_timesteps_;
+    x_veh_[0] = init_pose_[0];
+    y_veh_[0] = init_pose_[1];
+    theta_veh_[0] = init_pose_[2];
+    for (int i = 0; i < num_timesteps_-1; i++) {
+        error_[i] = (x_veh_[i] - ref_traj_[i][0])*(x_veh_[i] - ref_traj_[i][0]) +
+                   (y_veh_[i] - ref_traj_[i][1])*(y_veh_[i] - ref_traj_[i][1]) +
+                   (theta_veh_[i] - ref_traj_[i][2])*(theta_veh_[i] - ref_traj_[i][2]);
+        x_veh_[i+1] = x_veh_[i] + v[i]*cos(theta_veh_[i])*dt_;
+        y_veh_[i+1] = y_veh_[i] + v[i]*sin(theta_veh_[i])*dt_;
+        theta_veh_[i+1] = theta_veh_[i] + omega[i]*dt_;
+    }
 }
 
-void EgoVehicle::findOptimalTrajectory() {
-    // need:
-    //  1. obj fn - done
-    //  2. init condition - done
-    //  3. linear inequality/equality constraints - done
-    //  4. input lower+upper bounds - done
-    //  5. nonlinear constraints
+bool EgoVehicle::feasibleSolutionFound() {
+    return solution_found_;
 }
 
-OptimizationProblem::OptimizationProblem(Ipopt::Index num_timesteps, Eigen::Vector3f init_pose, double dt) : 
-                    num_timesteps_(num_timesteps), init_pose_(init_pose), dt_(dt) {        
-} 
-
-bool OptimizationProblem::get_nlp_info(Ipopt::Index& n, Ipopt::Index& m,
+bool EgoVehicle::get_nlp_info(Ipopt::Index& n, Ipopt::Index& m,
                       Ipopt::Index& nnz_jac_g, Ipopt::Index& nnz_h_lag,
                       Ipopt::TNLP::IndexStyleEnum& index_style) {
     n = 6*num_timesteps_;
-    m = 8*num_timesteps_ + 5;
-    nnz_jac_g = 12*num_timesteps_;
+    m = 5*num_timesteps_ + 5;
+    nnz_jac_g = 9*num_timesteps_;
     nnz_h_lag = 0;
     index_style = TNLP::C_STYLE;
     return true;
 }
 
-bool OptimizationProblem::get_bounds_info(Ipopt::Index n, double* x_l, double* x_u,
+bool EgoVehicle::get_bounds_info(Ipopt::Index n, double* x_l, double* x_u,
                          Ipopt::Index m, double* g_l, double* g_u) {
     for (int i = 0; i < 6; i++) {
         for (int j = 0; j < num_timesteps_; j++) {
@@ -77,13 +93,18 @@ bool OptimizationProblem::get_bounds_info(Ipopt::Index n, double* x_l, double* x
         }
     }
     for (int k = 0; k < m; k++) {
-        g_l[k] = -std::numeric_limits<Ipopt::Number>::infinity();
+        if (k < 5*num_timesteps_) {
+            g_l[k] = -std::numeric_limits<Ipopt::Number>::infinity();
+        }
+        else {
+            g_l[k] = 0;
+        }
         g_u[k] = 0;
     }
     return true;
 }
 
-bool OptimizationProblem::get_starting_point(Ipopt::Index n, bool init_x, double* x,
+bool EgoVehicle::get_starting_point(Ipopt::Index n, bool init_x, double* x,
                                bool init_z, double*, double*,
                                Ipopt::Index m, bool init_lambda, double*) {
     for (int i = 0; i < 6; i++) {
@@ -105,46 +126,107 @@ bool OptimizationProblem::get_starting_point(Ipopt::Index n, bool init_x, double
     return true;
 }
 
-bool OptimizationProblem::eval_f(Ipopt::Index n, const double* x, bool, double& obj_value) {
-    std::vector<double> cost_coeffs{0.1,0.1,0.01,100,1.5,0.1};
+bool EgoVehicle::eval_f(Ipopt::Index n, const double* x, bool, double& obj_value) {
     obj_value = 0;
     for (int i = 0; i < 6; i++) {
         for (int j = 0; j < num_timesteps_; j++) {
-            obj_value += cost_coeffs[i]*x[i*num_timesteps_ + j];
+            obj_value += cost_coeffs_[i]*x[i*num_timesteps_ + j];
         }
     }
     return true;
 }
 
-bool OptimizationProblem::eval_g(Ipopt::Index n, const double* x, bool new_x, Ipopt::Index m, double* g) {
-    std::vector<double> x_veh(num_timesteps_);
-    std::vector<double> y_veh(num_timesteps_);
-    std::vector<double> theta_veh(num_timesteps_);
-    x_veh[0] = init_pose_[0];
-    y_veh[0] = init_pose_[1];
-    theta_veh[0] = init_pose_[2];
-    double* v = x;
-    double* omega = x + num_timesteps_;
-    double* delta_e = x + 2*num_timesteps_;
-    double* delta_1 = x + 3*num_timesteps_;
-    double* delta_2 = x + 4*num_timesteps_;
-    double* delta_3 = x + 5*num_timesteps_;
-    for (int i = 0; i < num_timesteps_-1; i++) {
-        x_veh[i+1] = x_veh[i] + v[i]*cos(theta_veh[i])*dt_;
-        y_veh[i+1] = y_veh[i] + v[i]*sin(theta_veh[i])*dt_;
-        theta_veh[i+1] = theta_veh[i] + omega[i]*dt_;
+bool EgoVehicle::eval_g(Ipopt::Index n, const double* x, bool new_x, Ipopt::Index m, double* g) {
+    
+    computeTrajectory(x);
+    const double* v = x;
+    const double* omega = x + num_timesteps_;
+    const double* delta_e = x + 2*num_timesteps_;
+    const double* delta_1 = x + 3*num_timesteps_;
+    const double* delta_2 = x + 4*num_timesteps_;
+    const double* delta_3 = x + 5*num_timesteps_;    
+
+    for (int j = 0; j < num_timesteps_; j++) {
+        g[j] = 2*v[j]*((x_veh_[j] - ref_traj_[j][0])*cos(theta_veh_[j]) +
+                (y_veh_[j] - ref_traj_[j][1])*sin(theta_veh_[j])) + 
+                2*omega[j]*(theta_veh_[j] - ref_traj_[j][2]) + eps_*error_[j] - delta_e[j];
+        g[num_timesteps_ + j] = -(x_veh_[j] - ped_pose_[0])*(x_veh_[j] - ped_pose_[0])
+                                - (y_veh_[j] - ped_pose_[1])*(y_veh_[j] - ped_pose_[1])
+                                + r_*r_ - gamma_1_*(2*(x_veh_[j] - ped_pose_[0])*v[j]*cos(theta_veh_[j]) + 
+                                2*(y_veh_[j] - ped_pose_[1])*v[j]*sin(theta_veh_[j])) + delta_1[j];
+        g[2*num_timesteps_ + j] = v[j]*cos(theta_veh_[j]) - gamma_2_*(2 - y_veh_[j]) + delta_2[j];
+        g[3*num_timesteps_ + j] = -gamma_3_*(v[j] - 5) + delta_3[j];
+        g[4*num_timesteps_ + j] = 0.75 - y_veh_[j];
+    }
+    int index = 5*num_timesteps_;
+    Eigen::Vector3f end_pose = ref_traj_[num_timesteps_ - 1];
+    g[index] = x_veh_[num_timesteps_ - 1] - end_pose[0];
+    g[index + 1] = y_veh_[num_timesteps_ - 1] - end_pose[1];
+    g[index + 2] = theta_veh_[num_timesteps_ - 1] - end_pose[2];
+    g[index + 3] = v[num_timesteps_ - 1];
+    g[index + 4] = omega[num_timesteps_ - 1];
+    return true;
+}
+
+bool EgoVehicle::eval_grad_f(Ipopt::Index n, const double* x, bool new_x, double* grad_f) {
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < num_timesteps_; j++) {
+            grad_f[i*num_timesteps_ + j] = 2*cost_coeffs_[i]*x[i*num_timesteps_ + j];
+        }
     }
     return true;
 }
 
-void OptimizationProblem::finalize_solution(Ipopt::SolverReturn status,
+bool EgoVehicle::eval_jac_g(Ipopt::Index n, const double* x, bool,
+                       Ipopt::Index m, Ipopt::Index nele_jac,
+                       Ipopt::Index* iRow, Ipopt::Index* jCol,
+                       double* values) {
+    if (values == nullptr) {
+        // structure
+        for (int i = 0; i < num_timesteps_; i++) {
+            iRow[i] = i; jCol[i] = i;
+            iRow[num_timesteps_ + i] = i; jCol[num_timesteps_ + i] = num_timesteps_ + i;
+            iRow[2*num_timesteps_ + i] = i; jCol[2*num_timesteps_ + i] = 2*num_timesteps_ + i;
+            iRow[3*num_timesteps_ + i] = num_timesteps_ + i; jCol[3*num_timesteps_ + i] = i;
+            iRow[4*num_timesteps_ + i] = num_timesteps_ + i; jCol[4*num_timesteps_ + i] = 3*num_timesteps_ + i;
+            iRow[5*num_timesteps_ + i] = 2*num_timesteps_ + i; jCol[5*num_timesteps_ + i] = i;
+            iRow[6*num_timesteps_ + i] = 2*num_timesteps_ + i; jCol[6*num_timesteps_ + i] = 4*num_timesteps_ + i;
+            iRow[7*num_timesteps_ + i] = 3*num_timesteps_ + i; jCol[7*num_timesteps_ + i] = i;
+            iRow[8*num_timesteps_ + i] = 3*num_timesteps_ + i; jCol[8*num_timesteps_ + i] = 5*num_timesteps_ + i;
+        }
+    } else {
+        // values
+        computeTrajectory(x);
+        for (int i = 0; i < num_timesteps_; i++) {
+            values[i] = 2*(x_veh_[i] - ref_traj_[i][0])*cos(theta_veh_[i]) +
+                        2*(y_veh_[i] - ref_traj_[i][1])*sin(theta_veh_[i]);
+            values[num_timesteps_ + i] = 2*(theta_veh_[i] - ref_traj_[i][2]);
+            values[2*num_timesteps_ + i] = -1;
+            values[3*num_timesteps_ + i] = -gamma_1_*(2*(x_veh_[i] - ped_pose_[0])*cos(theta_veh_[i])
+                                            + 2*(y_veh_[i] - ped_pose_[1])*sin(theta_veh_[i]));
+            values[4*num_timesteps_ + i] = 1;
+            values[5*num_timesteps_ + i] = cos(theta_veh_[i]);
+            values[6*num_timesteps_ + i] = 1;
+            values[7*num_timesteps_ + i] = -gamma_3_;
+            values[8*num_timesteps_ + i] = 1;
+        }
+    }
+    return true;
+}
+
+void EgoVehicle::finalize_solution(Ipopt::SolverReturn status,
                            Ipopt::Index n, const double* x, const double* z_L,
                            const double* z_U, Ipopt::Index m,
                            const double* g, const double* lambda,
                            double obj_value,
                            const Ipopt::IpoptData* ip_data,
                            Ipopt::IpoptCalculatedQuantities* ip_cq) {
-
+    if (status == Ipopt::SolverReturn::SUCCESS) {
+        solution_found_ = true;
+    } else {
+        solution_found_ = false;
+        std::cerr << status << std::endl;
+    }
 }
 
 int main() {
--- rule_based_optimal_control.h
+++ rule_based_optimal_control.h
@@ -12,23 +12,12 @@ private:
     Eigen::Vector3f pose_{0.0F,0.0F,0.0F};
 };
 
-class EgoVehicle {
-public:
-    EgoVehicle();
-    ~EgoVehicle();
-    void setReferenceTrajectory(std::vector<Eigen::Vector3f>& ref_traj);
-    void findOptimalTrajectory();
-private:
-    float rear_axle_to_COG_;
-    float length_of_vehicle_;
-    std::vector<Agent*> agents_;
-    std::vector<Eigen::Vector3f> ref_traj_;
-    std::vector<std::vector<bool>> priority_structure_;
-};
-
-class OptimizationProblem : public Ipopt::TNLP {
-    OptimizationProblem(Ipopt::Index num_timesteps, Eigen::Vector3f init_pose, double dt);
-    ~OptimizationProblem() override {};
+class EgoVehicle : public Ipopt::TNLP {
+    EgoVehicle(Eigen::Vector3f init_pose, std::vector<Eigen::Vector3f>& ref_traj, 
+                std::vector<Agent*>& agents, double dt);
+    ~EgoVehicle() override;
+    void computeTrajectory(const double* x);
+    bool feasibleSolutionFound();
     bool get_nlp_info(Ipopt::Index& n, Ipopt::Index& m,
                       Ipopt::Index& nnz_jac_g, Ipopt::Index& nnz_h_lag,
                       Ipopt::TNLP::IndexStyleEnum& index_style) override;
@@ -43,14 +32,14 @@ class OptimizationProblem : public Ipopt::TNLP {
 
     bool eval_f(Ipopt::Index n, const double* x, bool new_x, double& obj_value) override;
 
-    // bool eval_grad_f(Ipopt::Index n, const double* x, bool new_x, double* grad_f) override;
+    bool eval_grad_f(Ipopt::Index n, const double* x, bool new_x, double* grad_f) override;
 
     bool eval_g(Ipopt::Index n, const double* x, bool new_x, Ipopt::Index m, double* g) override;
 
-    // bool eval_jac_g(Ipopt::Index n, const double* x, bool new_x,
-    //                 Ipopt::Index m, Ipopt::Index nele_jac,
-    //                 Ipopt::Index* iRow, Ipopt::Index* jCol,
-    //                 double* values) override;
+    bool eval_jac_g(Ipopt::Index n, const double* x, bool new_x,
+                    Ipopt::Index m, Ipopt::Index nele_jac,
+                    Ipopt::Index* iRow, Ipopt::Index* jCol,
+                    double* values) override;
 
     void finalize_solution(Ipopt::SolverReturn status,
                            Ipopt::Index n, const double* x, const double* z_L,
@@ -62,4 +51,21 @@ class OptimizationProblem : public Ipopt::TNLP {
     Ipopt::Index num_timesteps_;
     Eigen::Vector3f init_pose_;
     double dt_;
+    std::vector<Eigen::Vector3f> ref_traj_;
+    float rear_axle_to_COG_;
+    float length_of_vehicle_;
+    std::vector<Agent*> agents_;
+    std::vector<std::vector<bool>> priority_structure_;
+    double eps_;
+    double gamma_1_;
+    double gamma_2_;
+    double gamma_3_;
+    double r_;
+    std::vector<double> cost_coeffs_;
+    std::vector<double> x_veh_;
+    std::vector<double> y_veh_;
+    std::vector<double> theta_veh_;
+    std::vector<double> error_;
+    Eigen::Vector3f ped_pose_;
+    bool solution_found_;
 };
